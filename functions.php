f350d6a6bb231c6e45d1f535786442d31b538832
Ricard Torres <torres.rick@gmail.com>
2021-01-24 16:04:38 +0100

    1.3.2

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  *
  * @package link-manager
  */
-$theme_version = "1.3.1";
+$theme_version = "1.3.2";
 
 if (!defined("_S_VERSION")) {
   // Replace the version number of the theme on each release.
